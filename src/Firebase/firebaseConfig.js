cee99875382e29792b7b30e5be7267ca653fff9f
02Alexis <m_chael1012@hotmail.com>
2023-06-21 19:03:28 -0500

    commit

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkBFVAtQ8f-rmvg6CUw032w-BlPule_QE",
-  authDomain: "example-front4.firebaseapp.com",
-  projectId: "example-front4",
-  storageBucket: "example-front4.appspot.com",
-  messagingSenderId: "671096662584",
-  appId: "1:671096662584:web:f9d61ff191a3120af714cd",
-  // measurementId: "G-NT334YYGSB"
+  apiKey: YOUR_API_KEY,
+  authDomain: YOUR_AUTHDOMAIN,
+  projectId: YOUR_PROJECTID,
+  storageBucket: YOUR_STORAGEBUCKET,
+  messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+  appId: YOUR_APPID
 };
 
 // Initialize Firebase
